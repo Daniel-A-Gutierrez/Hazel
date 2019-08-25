c954166d9c0dc8fdfd77acee521138984b19c654
Daniel-A-Gutierrez <icanmakespaghetti@gmail.com>
2019-08-25 10:33:30 -0700

    Window Events!

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,14 +1,16 @@
 #include "hzpch.h"
 #include "Application.h"
-#include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
 #include "Events/Event.h"
 
 namespace Hazel
 {
+
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
@@ -21,6 +23,19 @@ namespace Hazel
 		{
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
+	void Application::OnEvent(Event & e)
+	{
+		HZ_TRACE("{0}", e);
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
 	}
 
 }
