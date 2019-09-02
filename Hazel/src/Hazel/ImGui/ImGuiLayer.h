a677a08ee461296370c590a0720134b055d8a0d9
Daniel-A-Gutierrez <icanmakespaghetti@gmail.com>
2019-09-02 06:59:24 -0700

    im gui events hack

--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include "Hazel/Layer.h"
+#include "Hazel/Events/Event.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Events/MouseEvent.h"
+
 
 namespace Hazel
 {
@@ -15,5 +20,25 @@ namespace Hazel
 		void OnEvent(Event& event);
 	private : 
 		float m_Time = 0.0f;
+
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent & e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent & e);
+		bool OnMouseMovedEvent(MouseMovedEvent & e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent & e);
+		bool OnKeyPressedEvent(KeyPressedEvent & e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent & e);
+		bool OnKeyTypedEvent(KeyTypedEvent & e);
+		bool OnWindowResizeEvent(WindowResizeEvent & e);
+
+
+
+
+
+
+
+
+
+
+
 	};
 }
